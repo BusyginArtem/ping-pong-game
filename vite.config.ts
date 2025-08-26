1e1b8fc0375c8182f1d9d1ff212d248a27d9ba5f
ArtemBusyhin <a.busyhin@urancompany.com>
2025-08-26 18:29:33 +0200

    optimize font

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,28 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import Unfonts from 'unplugin-fonts/vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      fontsource: {
+        families: [
+          {
+            name: 'Roboto',
+            weights: [400, 700],
+            styles: ['normal'],
+            subset: 'latin' 
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
